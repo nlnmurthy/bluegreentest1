6705e2cb58d8d47d9371f86ce7e2a17deabe940d
Muraliphanikumar Koneti -X (mkoneti - WIPRO LIMITED at Cisco) <mkoneti@partnet.cisco.com>
2018-12-28 12:47:05 +0530

    first commit

---
+++ pipeline/Jenkinsfile.prbuilderv1.groovy
@@ -0,0 +1,63 @@
+#!groovy
+
+def mergePullRequest() {
+    step([$class         : 'GhprbPullRequestMerge', 
+	allowMergeWithoutTriggerPhrase: false, 
+	deleteOnMerge: true,
+    disallowOwnCode: false, 
+	failOnNonMerge: true, 
+	mergeComment: 'Merged', 
+	onlyAdminsMerge: false])
+}
+
+try {
+
+    node() {
+
+
+        def mavenHome
+   		def currentDir = pwd()
+		def GitUtils = load("${currentDir}/utils/GitUtils.groovy")
+		def MiscUtils = load("${currentDir}/utils/MiscUtils.groovy")
+		def commitHash = GitUils.getCommitHash()
+		def changeLogSets = currentBuild.changeSets
+		def currentModules
+		
+        stage('Clone & Setup') {
+            checkout scm
+            mavenHome = tool(name: 'maven-3.5.0', type: 'maven');
+			
+			def changedModules = MiscUtils.getModifiedModules(changeLogSets)
+			def category = MiscUtils.getCategory(ghprbCommentBody)
+			currentModules = getCurrentModules(changedModules,category)
+			
+        }
+        withEnv([
+                'MAVEN_HOME=' + mavenHome,
+                "PATH=${mavenHome}/bin:${env.PATH}"
+        ]) {
+            stage('Build & UT') {
+                for (module in currentModules) {
+                    dir(module) {
+                        sh "'${mavenHome}/bin/mvn' clean test"
+                    }
+                }
+            }
+        }
+        stage('Merge') {
+            currentBuild.result = "SUCCESS"
+            if (ghprbCommentBody.startsWith("TM_MERGE")) {
+                node("ccone-slave") {
+                    checkout scm
+                    mergePullRequest()
+                }
+            }
+        }
+
+    }
+
+}
+catch (error) {
+    currentBuild.result = "FAILURE"
+    throw error
+}
