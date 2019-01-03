6d99434e9a1fd6309468140d44635794196500ee
Muraliphanikumar Koneti -X (mkoneti - WIPRO LIMITED at Cisco) <mkoneti@partnet.cisco.com>
2019-01-03 17:57:21 +0530

    first commit

--- pipeline/shelltest.groovy
+++ pipeline/shelltest.groovy
@@ -1,6 +1,15 @@
-node(){
+node{
+   stage('SCM Checkout'){
+     git https://github.com/muraliphani/bluegreentest1.git
+   }
+   stage('Compile-Package'){
+      // Get maven home path
+      def mvnHome = tool name: 'maven 3.6', type: 'maven' 
+      sh "${mvnHome}/bin/mvn package"
+   }
+   
 
-dir ('pipeline') { 
+  dir ('pipeline') { 
     sh('./test.sh')
 } 
 
