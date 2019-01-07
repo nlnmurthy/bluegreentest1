f163e2ca79b2ac30f211d717c0a3725bb68ff6a7
Muraliphanikumar Koneti -X (mkoneti - WIPRO LIMITED at Cisco) <mkoneti@partnet.cisco.com>
2019-01-07 18:11:44 +0530

    first commit

--- pipeline/shelltest.groovy
+++ pipeline/shelltest.groovy
@@ -7,7 +7,7 @@ node{
  dir('build'){
  
    sh('chmod +x notify-git.sh')
-   sh('./notify-git.sh "shashanp" "c2227ae0a98cc911d5c89007842d4a31a8ca743d" "https://sqbu-github.cisco.com" "shashanp" "test-trigger-project" 2 "this works i guess"')
+   sh('./notify-git.sh \"shashanp\" \"c2227ae0a98cc911d5c89007842d4a31a8ca743d\" \"https://sqbu-github.cisco.com\" \"shashanp\" \"test-trigger-project\" 2 \"this works i guess\"')
  
  }
  }
