611a64eeddf0607b43c64ad97e6be070764d9d8e
Muraliphanikumar Koneti -X (mkoneti - WIPRO LIMITED at Cisco) <mkoneti@partnet.cisco.com>
2019-01-16 23:52:01 +0530

    first commit

--- build/appdeploy.sh
+++ build/appdeploy.sh
@@ -9,10 +9,9 @@
  #   esac
 #done
 cf app devtest1-t >> cfappdetails.yml
-cat cfappdetails.yml
 HOST="$(awk '{if (NR==7) print$2}' cfappdetails.yml)"
 echo "$HOST"
-sed -i "2 s/url/${HOST}/g" devtest1-test\src\main\resources\application.yml
+sed -i "2 s/url/${HOST}/g" devtest1-test/src/main/resources/application.yml
 
 
 #sed -i "2s/$HOST/" org-management-test\src\main\resources\application.yml
