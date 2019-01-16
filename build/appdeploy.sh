f6d8acf10957a0cdf5e028b63c8cb0df0f9436c5
Muraliphanikumar Koneti -X (mkoneti - WIPRO LIMITED at Cisco) <mkoneti@partnet.cisco.com>
2019-01-16 23:48:00 +0530

    first commit

--- build/appdeploy.sh
+++ build/appdeploy.sh
@@ -8,7 +8,6 @@
   #      ;;
  #   esac
 #done
-cf login -a https://api.run.pivotal.io -u muralikoneti25@gmail.com -p Planttree_835
 cf app devtest1-t >> cfappdetails.yml
 cat cfappdetails.yml
 HOST="$(awk '{if (NR==7) print$2}' cfappdetails.yml)"
